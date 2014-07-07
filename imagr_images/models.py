5b5f1b92ea5d920d02ccc1b04e6a47e1bc3b2437
drytuna <drytuna@gmail.com>
2014-07-07 12:09:43 -0700

    created ImagrUser and defined friendship

--- imagr_images/models.py
+++ imagr_images/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
+from django.db.models import Q
 
 class Image(models.Model):
     image = models.FileField(upload_to='/photos/%Y/%m/%d')
@@ -39,8 +40,37 @@ class Relate(models.Model):
     user_1 = models.ForeignKey('auth.User', related_name='one')
     user_2 = models.ForeignKey('auth.User', related_name='two')
     relation = models.IntegerField(choices=((0, 'none'),
-                                            (1, '1 follow 2'),
-                                            (2, '2 follow 1'),
+                                            (1, '1 follows 2'),
+                                            (2, '2 follows 1'),
                                             (3, 'both')
                                             ))
-    friend = models.IntegerField(choices=((0, 'no'), (1, 'yes')))
+    friendship = models.IntegerField(choices=((0, 'no'),
+                                              (1, '1 request 2')
+                                              (2, '2 request 1')
+                                              (3, 'yes')
+                                              (4, '1 blocked 2')
+                                              (8, '2 blocked 1')
+                                              ))
+
+
+class ImagrUser(AbstractUser):
+
+    def followers(self):
+        iam_1 = (
+            Q(one__user_1 = self) &
+            Q(one__relation__in = [2, 3])
+            )
+        iam_2 = (
+            Q(two__user_2 = self) &
+            Q(two__relation__in = [1, 3])
+            )
+        my_followers = ImagrUser.objects.filter(
+            Q(iam_1 | iam_2)
+            )
+        return my_followers
+
+    def follow(self, other):
+        raise NotImplementedError
+
+    def unfollow(self, other):
+        raise NotImplementedError
