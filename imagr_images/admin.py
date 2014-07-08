26b08442132f3a464a30a98cbdd39950b9a6f472
Nathan Hubbell <lawrence.hubbell@gmail.com>
2014-07-07 18:09:53 -0700

    added simple classes to display read only fields

--- imagr_images/admin.py
+++ imagr_images/admin.py
@@ -1,7 +1,17 @@
 from django.contrib import admin
-from imagr_images.models import Image, Albumn, Relate
+from imagr_images.models import Image, Album, Relate
 # Register your models here.
 
-admin.site.register(Image)
-admin.site.register(Albumn)
+
+class ImageAdmin(admin.ModelAdmin):
+    readonly_fields = ('date_upl', 'date_mod')
+    list_display = ('title', 'owner', 'image')
+    list_filter = ('date_upl',)
+
+class AlbumAdmin(admin.ModelAdmin):
+    readonly_fields = ('date_upl', 'date_mod')
+    list_display = ('title', 'owner',)
+
+admin.site.register(Image, ImageAdmin)
+admin.site.register(Album, AlbumAdmin)
 admin.site.register(Relate)
